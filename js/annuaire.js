2b617a93a858e356bfa66da6f0cc11a8c52a8837
TotoSEO <TotoSEO@users.noreply.github.com>
2026-03-12 12:06:06 +0000

    deploy: 780ea085cfbdf6d40840b7138d3b282c2b3ac18e

--- js/annuaire.js
+++ js/annuaire.js
@@ -360,7 +360,7 @@
       // Update icon to user profile icon
       var svg = btn.querySelector('svg');
       if (svg) {
-        svg.innerHTML = '<path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4-4v2"/><circle cx="12" cy="7" r="4"/>';
+        svg.innerHTML = '<path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"/><circle cx="12" cy="7" r="4"/>';
       }
       // Update text
       var span = btn.querySelector('span');
